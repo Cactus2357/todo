688c7dbb5b2dbd7df1ec7029fef6ca27b60dcbed
BaoTQ <baotqhe181879@fpt.edu.vn>
2025-04-01 16:59:16 +0700

    update const

--- src/main/java/com/example/demo/constant/Const.java
+++ src/main/java/com/example/demo/constant/Const.java
@@ -1,6 +1,10 @@
 package com.example.demo.constant;
 
 public class Const {
+    /* Delete flg */
+    public static final String DELETE_FLG_0 = "0";
+    public static final String DELETE_FLG_1 = "1";
+
     /* Status type */
     public static final String STATUS_TYPE_USER = "user";
     public static final String STATUS_TYPE_ROLE = "role";
@@ -10,39 +14,39 @@ public class Const {
     public static final String STATUS_TYPE_SETTING = "setting";
 
     /* Status name id */
-    public static final String STATUS_USER_INACTIVE = "1";
-    public static final String STATUS_USER_ACTIVE = "2";
-    public static final String STATUS_USER_PENDING = "3";
-    public static final String STATUS_USER_BANNED = "4";
-    public static final String STATUS_USER_DELETED = "5";
-    public static final String STATUS_USER_SUSPENDED = "6";
-    public static final String STATUS_ROLE_INACTIVE = "7";
-    public static final String STATUS_ROLE_ACTIVE = "8";
-    public static final String STATUS_ROLE_PENDING = "9";
-    public static final String STATUS_ROLE_DEPRECATED = "10";
-    public static final String STATUS_ROLE_DELETED = "11";
-    public static final String STATUS_GROUP_INACTIVE = "12";
-    public static final String STATUS_GROUP_ACTIVE = "13";
-    public static final String STATUS_GROUP_ARCHIVED = "14";
-    public static final String STATUS_GROUP_PENDING = "15";
-    public static final String STATUS_GROUP_BANNED = "16";
-    public static final String STATUS_GROUP_DELETED = "17";
-    public static final String STATUS_USER_GROUP_INACTIVE = "18";
-    public static final String STATUS_USER_GROUP_ACTIVE = "19";
-    public static final String STATUS_USER_GROUP_PENDING = "20";
-    public static final String STATUS_USER_GROUP_BANNED = "21";
-    public static final String STATUS_USER_GROUP_LEFT = "22";
-    public static final String STATUS_USER_GROUP_ARCHIVED = "23";
-    public static final String STATUS_TASK_PENDING = "24";
-    public static final String STATUS_TASK_PROGRESS = "25";
-    public static final String STATUS_TASK_COMPLETED = "26";
-    public static final String STATUS_TASK_CANCELED = "27";
-    public static final String STATUS_TASK_ARCHIVED = "28";
-    public static final String STATUS_SETTING_INACTIVE = "29";
-    public static final String STATUS_SETTING_ACTIVE = "30";
-    public static final String STATUS_SETTING_PENDING = "31";
-    public static final String STATUS_SETTING_DEPRECATED = "32";
-    public static final String STATUS_SETTING_ARCHIVED = "33";
-    public static final String STATUS_SETTING_DELETED = "34";
+    public static final int STATUS_USER_INACTIVE = 1;
+    public static final int STATUS_USER_ACTIVE = 2;
+    public static final int STATUS_USER_PENDING = 3;
+    public static final int STATUS_USER_BANNED = 4;
+    public static final int STATUS_USER_DELETED = 5;
+    public static final int STATUS_USER_SUSPENDED = 6;
+    public static final int STATUS_ROLE_INACTIVE = 7;
+    public static final int STATUS_ROLE_ACTIVE = 8;
+    public static final int STATUS_ROLE_PENDING = 9;
+    public static final int STATUS_ROLE_DEPRECATED = 10;
+    public static final int STATUS_ROLE_DELETED = 11;
+    public static final int STATUS_GROUP_INACTIVE = 12;
+    public static final int STATUS_GROUP_ACTIVE = 13;
+    public static final int STATUS_GROUP_ARCHIVED = 14;
+    public static final int STATUS_GROUP_PENDING = 15;
+    public static final int STATUS_GROUP_BANNED = 16;
+    public static final int STATUS_GROUP_DELETED = 17;
+    public static final int STATUS_USER_GROUP_INACTIVE = 18;
+    public static final int STATUS_USER_GROUP_ACTIVE = 19;
+    public static final int STATUS_USER_GROUP_PENDING = 20;
+    public static final int STATUS_USER_GROUP_BANNED = 21;
+    public static final int STATUS_USER_GROUP_LEFT = 22;
+    public static final int STATUS_USER_GROUP_ARCHIVED = 23;
+    public static final int STATUS_TASK_PENDING = 24;
+    public static final int STATUS_TASK_PROGRESS = 25;
+    public static final int STATUS_TASK_COMPLETED = 26;
+    public static final int STATUS_TASK_CANCELED = 27;
+    public static final int STATUS_TASK_ARCHIVED = 28;
+    public static final int STATUS_SETTING_INACTIVE = 29;
+    public static final int STATUS_SETTING_ACTIVE = 30;
+    public static final int STATUS_SETTING_PENDING = 31;
+    public static final int STATUS_SETTING_DEPRECATED = 32;
+    public static final int STATUS_SETTING_ARCHIVED = 33;
+    public static final int STATUS_SETTING_DELETED = 34;
 
 }
