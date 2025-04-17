1404327a93abae34c4a8aed44bc01b91c537a447
BaoTQ <baotqhe181879@fpt.edu.vn>
2025-04-18 03:55:42 +0700

    add handle request validation

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,39 +1,74 @@
 package com.example.demo.exception;
 
 import com.example.demo.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    ResponseEntity<ApiResponse> handlingRuntimeException(Exception ex) {
+    ResponseEntity<ApiResponse<?>> handlingRuntimeException(Exception ex) {
         log.error(ex.getMessage(), ex);
-
         ErrorCode errorCode = ErrorCode.UNCATEGORIZED;
 
-        ApiResponse apiResponse = ApiResponse.builder()
-        .message(errorCode.getMessage())
-        .code(errorCode.getCode())
-        .build();
-
-        return ResponseEntity.internalServerError().body(apiResponse);
+        return ResponseEntity.internalServerError().body(ApiResponse.error(errorCode));
     }
 
     @ExceptionHandler(AppException.class)
-    ResponseEntity<ApiResponse> handlingAppException(AppException ex) {
+    ResponseEntity<ApiResponse<?>> handlingAppException(AppException ex) {
         ErrorCode errorCode = ex.getErrorCode();
 
-        ApiResponse apiResponse = ApiResponse.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build();
+        return ResponseEntity.status(errorCode.getStatusCode()).body(ApiResponse.error(errorCode));
+    }
 
-        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse<?>> handlingException(MethodArgumentNotValidException ex) {
+        ErrorCode errorCode = ErrorCode.UNCATEGORIZED;
+        Map<String, Object> attributes = null;
+
+        try {
+            errorCode = ErrorCode.valueOf(ex.getBindingResult().getFieldError().getDefaultMessage());
+
+            var constraintViolations = ex.getBindingResult().getAllErrors().getFirst()
+                    .unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolations.getConstraintDescriptor().getAttributes();
+
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        String message = Objects.nonNull(attributes) ? mapMessage(errorCode.getMessage(), attributes) : errorCode.getMessage();
+
+        return ResponseEntity.badRequest().body(ApiResponse.error(message, String.valueOf(errorCode.getCode())));
+
+    }
+
+    private String mapMessage(String message, Map<String, Object> attributes) {
+        if (message == null || attributes == null)
+            return message;
+
+        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (key == null || value == null) continue;
+
+            String placeholder = "{" + key + "}";
+            message = message.replace(placeholder, String.valueOf(value));
+        }
+
+        return message;
     }
 
 }
